86eec8b07b7cfda8159f3675c35dbfebc1893f51
Hakkenen <hkndourado@gmail.com>
2026-01-30 20:52:09 -0300

    Correção exercício 09 e 10

--- Python/Mundo1/PythonExercicios/exercicio009.py
+++ Python/Mundo1/PythonExercicios/exercicio009.py
@@ -1,12 +1,28 @@
-numero = int(input('Digite um número: '))
-print('Sua tabuada inteira é:',
-      '\n{} x 1 ='.format(numero), numero*1,
-      '\n{} x 2 ='.format(numero), numero*2,
-      '\n{} x 3 ='.format(numero), numero*3,
-      '\n{} x 4 ='.format(numero), numero*4,
-      '\n{} x 5 ='.format(numero), numero*5,
-      '\n{} x 6 ='.format(numero), numero*6,
-      '\n{} x 7 ='.format(numero), numero*7,
-      '\n{} x 8 ='.format(numero), numero*8,
-      '\n{} x 9 ='.format(numero), numero*9,
-      '\n{} x 10 ='.format(numero), numero*10)
+numero = int(input('Digite um número para ver sua tabuada: '))
+#print('Sua tabuada inteira é:',
+#      '\n{} x 1 ='.format(numero), numero*1,
+#'     '\n{} x 2 ='.format(numero), numero*2,
+#      '\n{} x 3 ='.format(numero), numero*3,
+#      '\n{} x 4 ='.format(numero), numero*4,
+#      '\n{} x 5 ='.format(numero), numero*5,
+#      '\n{} x 6 ='.format(numero), numero*6,
+#      '\n{} x 7 ='.format(numero), numero*7,
+#      '\n{} x 8 ='.format(numero), numero*8,
+#      '\n{} x 9 ='.format(numero), numero*9,
+#       '\n{} x 10 ='.format(numero), numero*10)
+# outra forma de resolver o exercício:
+# já formatado para melhor visualização
+print('-' * 12)
+print('{} x {:2} = {}'.format(numero, 1, numero*1))
+print('{} x {:2} = {}'.format(numero, 2, numero*2))
+print('{} x {:2} = {}'.format(numero, 3, numero*3))
+print('{} x {:2} = {}'.format(numero, 4, numero*4))
+print('{} x {:2} = {}'.format(numero, 5, numero*5))
+print('{} x {:2} = {}'.format(numero, 6, numero*6))
+print('{} x {:2} = {}'.format(numero, 7, numero*7))
+print('{} x {:2} = {}'.format(numero, 8, numero*8))
+print('{} x {:2} = {}'.format(numero, 9, numero*9))
+print('{} x {:2} = {}'.format(numero, 10, numero*10))
+print('-' * 12)
+
+#exercício feito com o conhecimento adquirido até a aula 07 do curso de Python do Curso em Vídeo
